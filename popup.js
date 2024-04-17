c573e2bf74f1f8a17ee672d4109b55d6b8d7758d
Shelby Cumings <s_c830@txstate.edu>
2024-04-17 13:43:10 -0500

    Returns groverprob

--- popup.js
+++ popup.js
@@ -6,7 +6,8 @@ async function onClick() {
     var sourceHTML = await getHTML();
     var message = document.querySelector('#message');
     articleText = getArticle(sourceHTML)
-    message.innerText = articleText;
+    queryAPI(articleText);
+    //message.innerText = articleText;
 }
 
 function getArticle(sourceHTML) {
@@ -50,6 +51,55 @@ function getHTML() {
             reject(error);
         });
     });
+ }
+
+ // Function to query API with article text
+function queryAPI(articleText) {
+    // Assemble request body
+    const requestBodyJSON = {
+        "article": articleText,
+        "authors": "",
+        "date": "",
+        "domain": "",
+        "title": "",
+        "target": "discrimination"
+    };
+    const requestBody = JSON.stringify(requestBodyJSON);
+
+    // Send POST request to API
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("POST", 'https://discriminate.grover.allenai.org/api/disc', false);
+    xmlHttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xmlHttp.send(requestBody);
+
+    // Handle API response
+    if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+        var jsonResponse = JSON.parse(xmlHttp.responseText);
+        var groverprob = jsonResponse.groverprob;
+        var result = "";
+        // Determine result based on groverprob
+        if (articleText == "none found") {
+            result = "We couldn't find an article on this page. ):";
+        } else if (groverprob >= 0 && groverprob <= 0.014853283762931824) {
+            result = "We believe this article is <str>human-written.</str>";
+        } else if (groverprob >= 0.014853283762931824 && groverprob <= 0.1) {
+            result = "This article is most likely <str>human-written.</str>";
+        } else if (groverprob >= 0.1 && groverprob <= 0.15) {
+            result = "This article may be human-written, but we're not sure.";
+        } else if (groverprob >= 0.8 && groverprob <= 0.85) {
+            result = "This article may be machine-written, but we're not sure.";
+        } else if (groverprob >= 0.85 && groverprob <= 0.9996635913848877) {
+            result = "This article is most likely <str>machine-written.</str>";
+        } else if (groverprob >= 0.9996635913848877 && groverprob <= 0.9999986886978149) {
+            result = "We believe this article is <str>human-written.</str>";
+        } else {
+            result = "We're not sure about this article.";
+        }
+        // Display the result
+        message.innerHTML = groverprob;
+    } else {
+        message.innerHTML = "<p>Error querying API.</p>";
+    }
 }
 
 function DOMtoString(selector) {
