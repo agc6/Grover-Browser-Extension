fac5646056fcc8b434c11725d9986c315d39252d
Shelby Cumings <s_c830@txstate.edu>
2024-04-17 11:58:28 -0500

    popup.js function
    Successfully divided operations into functions

--- popup.js
+++ popup.js
@@ -2,28 +2,33 @@ document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('clickButton').addEventListener('click', onClick);
 });
 
-function onClick() {
+async function onClick() {
+    var sourceHTML = await getHTML();
+    var message = document.querySelector('#message');
+    message.innerText = sourceHTML;
+}
+
+function getHTML() {
     var message = document.querySelector('#message');
     message.innerText = "Please wait a moment...";
 
-    chrome.tabs.query({ active: true, currentWindow: true }).then(function (tabs) {
-        var activeTab = tabs[0];
-        var activeTabId = activeTab.id;
+    return new Promise((resolve, reject) => {
+        chrome.tabs.query({ active: true, currentWindow: true }).then(function (tabs) {
+            var activeTab = tabs[0];
+            var activeTabId = activeTab.id;
 
-        return chrome.scripting.executeScript({
-            target: { tabId: activeTabId },
-            // injectImmediately: true,  // uncomment this to make it execute straight away, other wise it will wait for document_idle
-            func: DOMtoString,
-            // args: ['body']  // you can use this to target what element to get the html for
+            return chrome.scripting.executeScript({
+                target: { tabId: activeTabId },
+                func: DOMtoString
+            });
+
+        }).then(function (results) {
+            resolve(results[0].result);
+        }).catch(function (error) {
+            console.log('There was an error injecting script : \n' + error.message);
+            reject(error);
         });
-
-    }).then(function (results) {
-        var sourceHTML = results[0].result;
-        message.innerText = sourceHTML;
-    }).catch(function (error) {
-        message.innerText = 'There was an error injecting script : \n' + error.message;
     });
-
 }
 
 function DOMtoString(selector) {
