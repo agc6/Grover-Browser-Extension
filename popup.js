d7c4a2d4587cd654baae5896be4361b51661f31a
Shelby Cumings <s_c830@txstate.edu>
2024-04-17 11:39:00 -0500

    Pull source page HTML
    Altered extension title/description for accuracy, added loading message

--- popup.js
+++ popup.js
@@ -1,5 +1,10 @@
-function onWindowLoad() {
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('clickButton').addEventListener('click', onClick);
+});
+
+function onClick() {
     var message = document.querySelector('#message');
+    message.innerText = "Please wait a moment...";
 
     chrome.tabs.query({ active: true, currentWindow: true }).then(function (tabs) {
         var activeTab = tabs[0];
@@ -13,13 +18,13 @@ function onWindowLoad() {
         });
 
     }).then(function (results) {
-        message.innerText = results[0].result;
+        var sourceHTML = results[0].result;
+        message.innerText = sourceHTML;
     }).catch(function (error) {
         message.innerText = 'There was an error injecting script : \n' + error.message;
     });
-}
 
-window.onload = onWindowLoad;
+}
 
 function DOMtoString(selector) {
     if (selector) {
